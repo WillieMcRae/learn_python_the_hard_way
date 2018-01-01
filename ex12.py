72faca26d7a737a491686189670c3a6e0cd18531
Willie McRae <willie@williemcrae.com>
2018-01-01 10:21:08 +0000

    Exercise 12

---
+++ ex12.py
@@ -0,0 +1,7 @@
+# Cool to see how to get text inputs - good for text based games!!
+
+age = input("How old are you? ")
+height = input("How tall are you? ")
+weight = input("How much do you weigh? ")
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy")
