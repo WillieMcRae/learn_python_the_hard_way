91dfdd3c6d76cab75f64573b34d801f2ff0f4527
Willie McRae <willie@williemcrae.com>
2018-03-31 12:17:52 +0100

    Exercise 33

---
+++ ex33.py
@@ -0,0 +1,18 @@
+# While loops - be careful!
+
+i = 0
+numbers = [] # blank list
+
+while i < 6:
+    print(f"At the top i is {i}")
+    numbers.append(i)
+
+    i = i + 1
+    print("Numbers now: ", numbers)
+    print(f"At the bottom i is {i}")
+
+
+print("The numbers: ")
+
+for num in numbers:
+    print(num)
