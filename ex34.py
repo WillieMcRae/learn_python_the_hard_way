3f128e512cfccde04a3ef4ce3970a833850aed59
Will Bidstrup <will.bidstrup@gmail.com>
2018-04-03 16:00:44 +0100

    Exercise 34

---
+++ ex34.py
@@ -0,0 +1,12 @@
+# Accessing elements of lists
+animals = ['bear', 'python3.6', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+
+The animal at 1.# python3.6
+The third (3rd) animal. # peocock
+The first (1st) animal. # bear
+The animal at 3.# kangaroo
+The fifth (5th) animal. # whale
+The animal at 2. # peacock
+The sixth (6th) animal.# platypus
+The animal at 4. # whale
